29fc0a88d632521f0ca3b97e64e4368ff6aa1e01
duxm0114 <duxm0114@163.com>
2025-05-02 03:21:11 +0800

    引入dtm分布式事务

--- ruoyi-modules/ruoyi-workflow/src/main/java/com/ruoyi/workflow/service/impl/TaskViewService.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/com/ruoyi/workflow/service/impl/TaskViewService.java
@@ -5,6 +5,7 @@ import com.ruoyi.workflow.mapper.TaskViewMapper;
 import com.ruoyi.workflow.service.ITaskViewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pub.dtm.client.DtmClient;
 
 import java.util.List;
 
@@ -13,14 +14,19 @@ public class TaskViewService implements ITaskViewService {
 
     @Autowired
     private TaskViewMapper taskViewMapper;
+    @Autowired
+    DtmClient dtmClient;
 
     public TaskViewService() {
 
     }
 
-    public List<TaskView> getByAssigneeNamePage(String assigneeName , int pageNum, int pageSize) {
-         int offset = (pageNum - 1) * pageSize;
-         List<TaskView> users = taskViewMapper.selectByAssigneeNamePage(assigneeName, pageSize, offset);
+    public List<TaskView> getByAssigneeNamePage(String assigneeName, int pageNum, int pageSize) {
+        if (dtmClient != null) {
+            System.out.println(" dtm client is ok \n");
+        }
+        int offset = (pageNum - 1) * pageSize;
+        List<TaskView> users = taskViewMapper.selectByAssigneeNamePage(assigneeName, pageSize, offset);
         return users;
     }
 
