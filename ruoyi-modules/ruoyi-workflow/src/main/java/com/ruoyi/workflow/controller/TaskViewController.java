bf8dd6e769ef9455aa9845800a9c060958801d7c
andy <duxm0114@163.com>
2025-05-06 14:09:43 +0800

    rsql模式bug修改。

--- ruoyi-modules/ruoyi-workflow/src/main/java/com/ruoyi/workflow/controller/TaskViewController.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/com/ruoyi/workflow/controller/TaskViewController.java
@@ -10,7 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.ruoyi.common.rsql.sql.SQLProcess;
+import com.ruoyi.common.rsql.Process;
 import java.util.List;
 
 /**
@@ -28,7 +29,12 @@ public class TaskViewController extends BaseController {
      * 获取参数配置列表
      */
     @GetMapping("/list")
-    public PageInfo<TaskView> list() {
+    public PageInfo<TaskView> list() throws Exception {
+        SQLProcess proc = new SQLProcess("test");
+        Process.parse("name=='lxd' and name!='lxd'", proc);
+        String sql = proc.getSQL();
+        System.out.println(sql);
+
         PageInfo<TaskView> list = taskViewService.getByAssigneeName("张三",0, 100);
         return  list;
     }
